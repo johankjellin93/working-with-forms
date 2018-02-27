5c237b97bb67e4cbf2fca77f50452e18624d03cb
Unknown <johankjellin93@gmail.com>
2018-02-27 19:01:01 +0100

    disable a field

--- Exercise Files/Ch03/03_07/script.js
+++ Exercise Files/Ch03/03_07/script.js
@@ -1,12 +1,28 @@
 (function() {
 "use strict";
 
-document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
+var state = document.getElementById('s-state');
+
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
+
+	var btnEstimate = document.getElementById('btn-estimate');
+
+	btnEstimate.disabled = true;
+
+	state.addEventListener('change', function() {
+
+		if (state.value === '') {
+			btnEstimate.disabled = true;
+		} else {
+			btnEstimate.disabled = false;
+		}
+		
+	});
+});
 
 function estimateTotal(event) {
 	event.preventDefault();
-	
-	var state = document.getElementById('s-state');
 	
 	if (state.value === '') {
 		alert('Please choose your shipping state.');
